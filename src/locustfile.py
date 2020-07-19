861986eeeedbc287ecb5cc01898dc1f6aa0b09c9
Leticia Araújo <leticiaaraujo@Leticias-MacBook-Air.local>
2020-07-19 15:34:26 -0300

    inicial project locust

---
+++ src/locustfile.py
@@ -0,0 +1,59 @@
+from locust import HttpUser, SequentialTaskSet, task, between, events
+import time
+import json
+
+class MyTaskSet(SequentialTaskSet):
+    
+    @task
+    def list_of_users(self):
+        h={'Content-Type': 'application/json'}
+        chosen_page = '1'
+        response = self.client.get("/users?page={chosen_page!s}".format(**locals()), headers=h)
+        json_var = response.json()
+        print("List of users status code: ", response.status_code)
+        print(json_var)
+        assert response.status_code == 200
+
+    @task
+    def successlly_login(self):
+        successlly_payload = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
+        h={'Content-Type': 'application/json'}
+        response = self.client.post("/login", data=json.dumps(successlly_payload), headers=h)
+        json_var = response.json()
+        print("Successlly login status code: ", response.status_code)
+        print(json_var)
+        assert response.status_code == 200
+
+    @task
+    def unsuccesslly_login(self):
+        unsuccesslly_payload = {"email": "eve.holt@reqres.in"}
+        h={'Content-Type': 'application/json'}
+        response = self.client.post("/login", data=json.dumps(unsuccesslly_payload), headers=h)
+        json_var = response.json()
+        print("Unsuccesslly login status code: ", response.status_code)
+        print(json_var)
+        assert response.status_code == 400
+
+    @task
+    def update_user(self):
+        update_payload = {"name": "morpheus", "job":"zion resident"}
+        h={'Content-Type': 'application/json'}
+        response = self.client.put("/users", data=json.dumps(update_payload), headers=h)
+        json_var = response.json()
+        print("Update user status code: ", response.status_code)
+        print(json_var)
+        assert response.status_code == 200
+
+    @task
+    def create_user(self):
+        create_payload = {"name": "aiana", "job":"singer"}
+        h={'Content-Type': 'application/json'}
+        response = self.client.post("/users", data=json.dumps(create_payload), headers=h)
+        json_var = response.json()
+        print("Create user status code: ", response.status_code)
+        print(json_var)
+        assert response.status_code == 201
+    
+class MyUser(HttpUser):
+    tasks = [MyTaskSet]
+    wait_time = between(5, 20)
